5e9e00664c943b1d1314b90b5f88ff047e9076fb
sassysiya <119407449+sassysiya@users.noreply.github.com>
2023-01-30 22:16:01 +0530

    Update q3.py

--- q3.py
+++ q3.py
@@ -1,13 +1,4 @@
-a = int(input("Enter number of terms you want in list: "))
-x = []
-for i in range(0,a):
-    d = int(input(f"Enter {i+1} th item of list: " ))
-    x.append(d)
-x.sort()
-y = []
-for j in range(0,a):
-    g = x[j]**2
-    y.append(g)
-
-list_tuple = list(zip(x,y))
-print(list_tuple)
+l = int(input("Enter lower limit: "))
+u = int(input("Enter upper limit: "))
+a = [(x,x**2) for x in range(l,u+1)]
+print (a)
